690c8c953d63e8a97672aa9dfd33c7ad34ea3531
Andrey Kovalevskiy <andkov18@gmail.com>
2023-12-08 18:02:30 +0100

    Added an image that updates each time user selects new image to upload

--- pages/profile.js
+++ pages/profile.js
@@ -3,6 +3,9 @@ import ProfilePicture from '../components/ProfilePicture';
 import { Profile } from "../components/Profile";
 import { useState } from "react";
 import { useUserdata } from "../lib/useUserdata";
+import Image from "next/image";
+import utilStyles from '../styles/utils.module.css';
+import { read } from "gray-matter";
 
 export default function ProfilePage() {
   // ToDo: add a nice layout, display the current display name, register date, etc. (and the avatar of course)
@@ -13,9 +16,15 @@ export default function ProfilePage() {
   const user = useUserdata({ caller: "ProfilePage" });
 
   const [avatarFile, setAvatarFile] = useState(null);
+  const [avatarFromReader, setAvatarFromReader] = useState(null);
 
   function onAvatarInputChange(ev) {
     setAvatarFile(ev.target.files[0]);
+    let reader = new FileReader();
+    reader.onload = function() {
+      setAvatarFromReader(reader.result);
+    }
+    reader.readAsDataURL(ev.target.files[0]);
   }
 
   async function onSubmit(ev) {
@@ -38,6 +47,7 @@ export default function ProfilePage() {
         <h1>You are logged in as {user?.user ? user.user.username : "error" }</h1>
         {console.log("user requested") == null && user?.user && true ? <Profile userId={user.user.id} /> : null}
       </div>
+      <Image id="pic" src={avatarFromReader ? avatarFromReader : "/images/forest-small.png"} width="160" height="160" className={utilStyles.borderCircle} />
       <form method="POST" action="/api/edit-profile-avatar" enctype="multipart/form-data">
         <input type="file" name="avatar" accept="image/png" onChange={onAvatarInputChange} />
         <p>{avatarFile?.name}</p>
