667c9231516d4f0a2bb722cb6bc30304a61a1286
Andrey Kovalevskiy <andkov18@gmail.com>
2023-12-08 20:41:14 +0100

    Now the profile page displays the user's avatar that can be changed by clicking the image. It gets updated instantly, without the need to update the page

--- pages/profile.js
+++ pages/profile.js
@@ -1,46 +1,39 @@
 import StringInput from "../components/StringInput";
 import ProfilePicture from '../components/ProfilePicture';
 import { Profile } from "../components/Profile";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { useUserdata } from "../lib/useUserdata";
 import Image from "next/image";
 import utilStyles from '../styles/utils.module.css';
-import { read } from "gray-matter";
 
 export default function ProfilePage() {
   // ToDo: add a nice layout, display the current display name, register date, etc. (and the avatar of course)
   // and add the ability to edit the display name and the password
 
-  console.log("redraw started");
-
   const user = useUserdata({ caller: "ProfilePage" });
 
-  const [avatarFile, setAvatarFile] = useState(null);
-  const [avatarFromReader, setAvatarFromReader] = useState(null);
+  const [newAvatarSerialized, setNewAvatarSerialized] = useState(null);
 
-  function onAvatarInputChange(ev) {
-    setAvatarFile(ev.target.files[0]);
-    let reader = new FileReader();
-    reader.onload = function() {
-      setAvatarFromReader(reader.result);
-    }
-    reader.readAsDataURL(ev.target.files[0]);
-  }
+  const fileInputRef = useRef(null);
 
-  async function onSubmit(ev) {
-    ev.preventDefault();
+  async function onAvatarInputChange(ev) {
     const formData = new FormData();
-    console.log(avatarFile);
-    formData.append('avatar', avatarFile, avatarFile.name);
-    fetch('/api/edit-profile-avatar', {
+    console.log(ev.target.files[0]);
+    formData.append('avatar', ev.target.files[0], ev.target.files[0].name);
+    const success = await fetch('/api/edit-profile-avatar', {
       method: 'POST',
       headers: { },
       body: formData,
     });
+    console.log("success", success);
+    if (success.status == 200) {
+      let reader = new FileReader();
+      reader.onload = function() {
+        setNewAvatarSerialized(reader.result);
+      }
+      reader.readAsDataURL(ev.target.files[0]);
+    }
   }
-
-  console.log("redraw finishing");
-  console.log("profile user", user?.user?.id);
 
   return (
     <>
@@ -48,22 +41,20 @@ export default function ProfilePage() {
         <h1>You are logged in as {user?.user ? user.user.username : "error" }</h1>
         {console.log("user requested") == null && user?.user && true ? <Profile userId={user.user.id} /> : null}
       </div>
-      {
-        avatarFromReader ? 
+      <a onClick={() => fileInputRef.current.click()}>{
+        newAvatarSerialized ? 
         <Image
-          src={avatarFromReader}
+          src={newAvatarSerialized}
           alt="Profile Picture"
           width={160}
           height={160}
           className={utilStyles.borderCircle}
         /> :
-        <ProfilePicture userId={user?.user ? user.user.id : "error"} size={160} />
-      }
+        <ProfilePicture userId={user?.user ? user.user.id : 0} size={160} />
+      }</a>
       
-      <form method="POST" action="/api/edit-profile-avatar" enctype="multipart/form-data">
-        <input type="file" name="avatar" accept="image/png" onChange={onAvatarInputChange} />
-        <p>{avatarFile?.name}</p>
-        <button type="submit" onClick={onSubmit}>Upload</button>
+      <form method="POST" action="/api/edit-profile-avatar" enctype="multipart/form-data" style={{ display: "none" }}>
+        <input type="file" ref={fileInputRef} name="avatar" accept="image/png" onChange={onAvatarInputChange} />
       </form>
     </>
   );
